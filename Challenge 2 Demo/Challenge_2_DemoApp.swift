fb77417f3c4fc0d5a2fa2a081a669e42f4970a22
Tiffany Lim <tiffamon.the.dasher@gmail.com>
2026-07-04 14:22:30 +0800

    Initial Commit

---
+++ Challenge 2 Demo/Challenge_2_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2_DemoApp.swift
+//  Challenge 2 Demo
+//
+//  Created by Tiffany Lim on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
