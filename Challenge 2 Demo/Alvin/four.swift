15eca6157f3f014a447892a232429998aa1ed07c
Alvin Phyo Htet <alvinpt100@gmail.com>
2026-08-07 14:29:42 +0800

    Added a new swift view file called "four"

---
+++ Challenge 2 Demo/Alvin/four.swift
@@ -0,0 +1,18 @@
+//
+//  four.swift
+//  Challenge 2 Demo
+//
+//  Created by Alvin Phyo Htet on 7/8/26.
+//
+
+import SwiftUI
+
+struct four: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    four()
+}
